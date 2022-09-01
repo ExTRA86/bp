4b29bfbbf750887bdfa6d13507631965601c120b
TuishevMarat <tuishev-marat@yandex.ru>
2022-09-01 19:34:57 +0300

    Добавил картинки

--- src/pages/Photo.jsx
+++ src/pages/Photo.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Photo = () => {
+  const [photos, setPhotos] = useState([]);
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/photos`)
+      .then(response => response.json())
+      .then(json => setPhotos(json.slice(0, 10)));
+  }, []);
+  console.log(photos);
+
   return (
     <div>
-      <h1>Photo</h1>
+      <h1>Photo 2</h1>
+      {photos.map(photo => (
+        <div>
+          <img src={photo.thumbnailUrl} alt='' key={photo.id}></img>
+          <span>{photo}</span>
+        </div>
+      ))}
     </div>
   );
 };
