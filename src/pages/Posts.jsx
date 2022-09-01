d3222837684b32f7f1e9a375c6b1e40eac1073c6
ExTRA86 <raf77786@gmail.com>
2022-09-01 21:30:18 +0300

    настроил отображение постов

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,8 +1,10 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import './PostStyle.css';
 
 const Posts = () => {
   const [posts, setPosts] = useState([]);
+  const [comments, setComments] = useState([]);
 
   useEffect(() => {
     // fetch(`https://jsonplaceholder.typicode.com/posts`)
@@ -20,18 +22,43 @@ const Posts = () => {
         const allPosts = response.data;
         setPosts(allPosts);
       });
+
+    axios
+      .get(`https://jsonplaceholder.typicode.com/comments`, {
+        params: {
+          _limit: 10,
+          _page: 1,
+        },
+      })
+      .then(response => {
+        const allComments = response.data;
+        setComments(allComments);
+      });
     // eslint-disable-next-line
   }, []);
 
-  console.log(posts);
+  // console.log(posts);
+  // console.log(comments[0]);
   return (
-    <div>
-      <h1>Posts</h1>
-      <ul>
-        {posts.map((post, index) => (
-          <li key={index}>{post.title}</li>
+    <div className='posts'>
+      <h2>Posts</h2>
+      <div className='post__list'>
+        {posts.map(post => (
+          <div key={post.id} className='post__item'>
+            <div>
+              <strong>Title: </strong>
+              {post.title}
+            </div>
+            <div>
+              <strong>Body: </strong> {post.body}
+            </div>
+            <div>
+              <strong>Comments: </strong>
+              {comments[post.id - 1].name}
+            </div>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 };
