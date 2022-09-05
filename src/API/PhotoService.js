b728112521114a9f61545d521626ca8a76b642a6
TuishevMarat <tuishev-marat@yandex.ru>
2022-09-05 21:42:26 +0300

    Сделал класс для картинок

---
+++ src/API/PhotoService.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+export default class PhotoService {
+  static async getAll(limit = 10, page = 1) {
+    const response = await axios.get(
+      `https://jsonplaceholder.typicode.com/photos`,
+      {
+        params: {
+          _limit: limit,
+          _page: page,
+        },
+      },
+    );
+    return response;
+  }
+
+  static async getById(id) {
+    const response = await axios.get(
+      `https://jsonplaceholder.typicode.com/photos` + id,
+    );
+    return response;
+  }
+
+  static async getTitleByPhotoId(id) {
+    const response = await axios.get(
+      `https://jsonplaceholder.typicode.com/photos/${id}/url`,
+    );
+    return response;
+  }
+}
--- src/pages/Photo.jsx
+++ src/pages/Photo.jsx
@@ -1,20 +1,40 @@
 import React, { useEffect, useState } from 'react';
+
 import './PhotoStyle.css';
 import Modal from '../components/UI/Modal';
+import PhotoService from '../API/PhotoService';
 
 const Photo = () => {
   const [photos, setPhotos] = useState([]);
   const [modal, setModal] = useState(false);
   const [current, setCurrent] = useState();
+  const [limit, setLimit] = useState(10);
+  const [page, setPage] = useState(1);
+  const changePage = page => {
+    setPage(page);
+    fetchPics(photos.url);
+    console.log(page);
+  };
+
   const bigImg = img => {
     setModal(true);
     setCurrent(img);
   };
+
+  async function fetchPics(limit, page) {
+    const pics = await PhotoService.getAll(limit, page);
+    setPhotos(pics.data);
+  }
+
   useEffect(() => {
-    fetch(`https://jsonplaceholder.typicode.com/photos`)
-      .then(response => response.json())
-      .then(json => setPhotos(json.slice(0, 10)));
-  }, []);
+    fetchPics(limit, page);
+  }, [limit, page]);
+
+  // useEffect(() => {
+  //   fetch(`https://jsonplaceholder.typicode.com/photos`)
+  //     .then(response => response.json())
+  //     .then(json => setPhotos(json.slice(0, 10)));
+  // }, []);
 
   return (
     <div className='photos'>
@@ -23,18 +43,14 @@ const Photo = () => {
       </div>
       <div className='list'>
         {photos.map(photo => (
-          <div
-            key={photo.id}
-            className='pic'
-            onClick={() => bigImg(photo.url, photo.title)}
-          >
+          <div key={photo.id} className='pic' onClick={() => bigImg(photo.url)}>
             <img className='smallImg' src={photo.thumbnailUrl} alt='' />
           </div>
         ))}
         <Modal visible={modal} setVisible={setModal}>
           <div>
             <img className='bigImg' src={current} alt='' />
-            <button></button>
+            <button onClick={() => changePage(page + 1)}>Next</button>
           </div>
         </Modal>
       </div>
